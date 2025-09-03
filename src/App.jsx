68dc782da04fe973a796d35c7043c9879d3ec10c
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-09-03 12:26:56 +0300

    Handling arrays is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,105 @@ import "./App.css";
 
 import { useState } from "react";
 
+//Display component
 const Display = ({ counter }) => 
 <p className="counterin-arvo">{counter}</p>;
 
+//Button component
 const Button = ({ onClick, text }) => 
 <button className="nappi" onClick={onClick}>{text}</button>;
 
-const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log("Rendering with default counter value", counter);
+//history component
+const History = ({ history }) => {
+  if (history.length === 0) {
+    return (
+      <p>
+        <span className="note">Note:</span> No button is pressed yet!
+      </p>
+    );
+  }
 
+  return <p>History: {history.join(" ")}</p>;
+};
+
+
+
+ 
+
+const App = () => {
+
+  //main counter + history:
+
+  const [counter, setCounter] = useState(0);
+        console.log("Rendering with default counter value", counter);
+  //Add a new state array for history
+  const [history, setHistory] = useState([]);
+
+  //left-right counters
+  const [clicks, setClicks] = useState({left: 0, right:0});
+
+  //Array to track all clicks (array task)
+  const [allClicks, setAllClicks] = useState([]);
+
+    //Main Handlers - counter:
   const increaseByOne = () => {
-    console.log("Increaseing, value before", counter);
-    setCounter(counter + 1);
+        console.log("Increaseing, value before", counter);
+        setCounter(counter + 1);
+        setHistory(history.concat("plus"));//add "plus" to history
   };
   const decreaseByOne = () => {
-    console.log("Decreasing, value before", counter);
-    setCounter(counter - 1);
+        console.log("Decreasing, value before", counter);
+        setCounter(counter - 1);
+        setHistory(history.concat("Minus"));
   };
   const setToZero = () => {
-    console.log("Resetting to Zero, value before", counter);
-    setCounter(0);
+        console.log("Resetting to Zero, value before", counter);
+        setCounter(0);
+        setHistory(history.concat("Zero"));
   };
 
+  //Left-Right handlers
+  const handleLeftClick =() => {
+  setClicks({...clicks, left: clicks.left + 1});
+  setAllClicks(allClicks.concat("L"));
+  };
+  
+  const handleRightClick = () => {
+  setClicks({...clicks, right:clicks.right + 1});
+  setAllClicks(allClicks.concat("R"));
+};
+ 
+
   return (
-    <>
-      <Display counter = {counter}/>
+<div>
+      {/* Left/Right counters */}
+      <div className="two-counters">
+        <p>Left counter: <Display counter={clicks.left} /></p>
+        <Button onClick={handleLeftClick} text="Left" />
 
-      <Button onClick={increaseByOne} text="Plus"/>
-      <Button onClick={setToZero} text="Zero"/>
-      <Button onClick={decreaseByOne} text="Minus"/>
+        <p>Right counter: <Display counter={clicks.right} /></p>
+        <Button onClick= {handleRightClick} text="Right" />
+      </div>
 
+      <hr />
 
-      
-    </>
+    {/* Main counter */}
+      <p>Main counter: <Display counter={counter} /></p>
+      <Button onClick={increaseByOne} text="Plus" />
+      <Button onClick={setToZero} text="Zero" />
+      <Button onClick={decreaseByOne} text="Minus" />
+
+      <hr />
+
+            {/* History */}
+      <History history={history} />
+      <hr />
+      {/* All clicks array */}
+      <p>All clicks: {allClicks.join("")}</p>
+    </div>
   );
 };
+   
+
 
 export default App;
